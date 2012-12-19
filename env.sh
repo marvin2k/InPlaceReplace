a53e383b611b900d6f03dfcc4914978afdd0ed33
Martin Zenzes (RH5) <martin.zenzes@dfki.de>
2012-12-19 13:41:39 +0100

    some work on the readme, adding new extensive documentation link, adding PATH to env.sh
    because it quickly gets lost in all the changes, the documentation link is
        http://clang.llvm.org/docs/index.html

--- env.sh
+++ env.sh
@@ -2,5 +2,6 @@
 
 # needed to actually compile all this using clang, and to use the python bindings.
 
+export PATH=$HOME/llvm.install/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/llvm.install/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$HOME/llvm.git/tools/clang/bindings/python/clang:$PWD/python:$PYTHONPATH
